f4d892dd30b57b0bfe1b4241c8b2e5d8bcdae6d0
SaneMyburg <sane.myburg@gmail.com>
2023-04-08 05:34:31 +0200

    Add missing JSX elements

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -30,13 +30,31 @@ const DisplayBooks = () => {
             books.map((book) => (
               <li key={book.id}>
                 <Book book={book} />
+                <div className="progress-container">
+                  <div role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax={100} aria-label="reading-progess" />
+                  <div className="reading-progress">
+                    <p className="complete-reading">
+                      {Math.floor(Math.random() * (99 - 0)) + 0}
+                      <span>%</span>
+                    </p>
+                    <p className="completed">Completed</p>
+                  </div>
+                </div>
+                <div className="side-border" />
+                <div className="chapter-status">
+                  <p>CURRENT CHAPTER</p>
+                  <p>
+                    Chapter
+                    {' '}
+                    {Math.floor(Math.random() * (20 - 0)) + 0}
+                  </p>
+                  <button type="button" className="update-btn">UPDATE PROGRESS</button>
+                </div>
               </li>
             ))
           }
           </ul>
-
         </div>
-
       </div>
     </>
   );
